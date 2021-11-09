ef4fd373e254a01419e81f1b21b682ddfaf49f75
Łukasz Gemborowski <lukasz.gemborowski@gmail.com>
2021-11-09 23:36:31 +0100

    example: small refactor

--- example/rpc_server_client.cxx
+++ example/rpc_server_client.cxx
@@ -17,19 +17,23 @@ struct SimpleApi
         stop
     };
 
-    // create API object for use in emb::rpc subsystem
-    static constexpr auto make() {
-        using namespace emb::rpc;
-        return make_api(
-            // map function ID to function signature
-            procedure<Function::add,            int  (int, int)>{},
-            procedure<Function::mul,            int  (int, int)>{},
-            procedure<Function::print,          void (std::string)>{},
-            procedure<Function::stop,           void ()>{}
-        );
-    }
+    // a shortcut, with this small util we don't need to write emb::rpc::procedure everytime,
+    // proc<> wil be enough in this "scope"
+    template<auto Id, class T>
+    using proc = emb::rpc::procedure<Id, T>;
+
+    // define an API
+    using definition = emb::rpc::api<
+        // this is mapping from ID to a function prototype, eg Function::add is function accepting
+        // two int's as argument and returning single int
+        proc<Function::add,            int  (int, int)>,
+        proc<Function::mul,            int  (int, int)>,
+        proc<Function::print,          void (std::string)>,
+        proc<Function::stop,           void ()>
+    >;
 };
 
+// Example RPC server implementation
 class Server
 {
 public:
@@ -37,13 +41,21 @@ public:
         : socket_ {socket_path}
         , rpc_ {socket_}
     {
+        // register callbacks for API functions, lambda functions must match defined API,
+        // therefore we're just delegating calls to member functions ...
         rpc_.callback<SimpleApi::Function::add>() = [this](int a, int b) { return add(a, b); };
         rpc_.callback<SimpleApi::Function::mul>() = [this](int a, int b) { return mul(a, b); };
         rpc_.callback<SimpleApi::Function::print>() = [this]( auto const& str) { return print(str); };
-        rpc_.callback<SimpleApi::Function::stop>() = [this] { stop(); };
+
+        // ... or write a callback completly as lambda
+        rpc_.callback<SimpleApi::Function::stop>() = [this] {
+            std::cout << "Stop requested." << std::endl;
+            rpc_.stop_running();
+        };
     }
 
     void run() {
+        // run a server in separate thread
         thread_ = std::thread { [this] { rpc_.run(); } };
     }
 
@@ -64,13 +76,9 @@ private:
         std::cout << "got string: " << str << std::endl;
     }
 
-    void stop() {
-        rpc_.stop_running();
-    }
-
 private:
     emb::net::uds_server_socket socket_;
-    emb::rpc::socket_server<decltype(SimpleApi::make())> rpc_;
+    emb::rpc::socket_server<SimpleApi::definition> rpc_;
     std::thread thread_;
 };
 
@@ -79,20 +87,25 @@ int main()
     using namespace std::string_literals;
 
     const char* socket_name = "/tmp/test.sock";
+
+    // create a server
     Server srv{socket_name};
 
     // create and connect RPC client
     emb::net::uds_client_socket cs{socket_name};
-    auto cli = emb::rpc::make_socket_client(SimpleApi::make(), cs);
+    emb::rpc::socket_client<SimpleApi::definition> cli{cs};
 
     // run the server in its own thread
     srv.run();
 
-    // remotely call the server API
+    // remotely call the server API, the call<ID>() will block until the result is received
+    // therefore we can use them almost the same way as "local" functions
     std::cout << "Remote add result (40 + 2) = " << cli.call<SimpleApi::Function::add>(40, 2) << std::endl;
     std::cout << "Remote mul result (21 * 2) = " << cli.call<SimpleApi::Function::mul>(21, 2) << std::endl;
     cli.call<SimpleApi::Function::print>("Hello server!"s);
 
     cli.call<SimpleApi::Function::stop>();
+
+    // wait for theserver thread to join
     srv.join();
 }
